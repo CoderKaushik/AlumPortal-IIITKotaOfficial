353fb8b225f5b2760437372359e83d508db391c8
Coder Kaushik <2022kucp1077@iiitkota.ac.in>
2024-08-31 02:10:02 +0530

    ,,,

--- server/routes/passwordRoutes.js
+++ server/routes/passwordRoutes.js
@@ -19,7 +19,7 @@ router.post("/forgot-password", async (req, res) => {
     await user.save();
 
     // const resetUrl = `http://localhost:5000/reset-password/${token}`;
-    const resetUrl = `https://alumportal-iiitkotaofficial.onrender.com/reset-password/${token}`;
+    const resetUrl = `https://alumportal-iiitkotaofficial.onrender.com/api/password/reset-password/${token}`;
 
     const message = `
       <h1>You have requested a password reset</h1>
