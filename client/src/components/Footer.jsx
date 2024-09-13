5528fffaa603d0278480eabf280edcfe6f776cc1
Coder Kaushik <2022kucp1077@iiitkota.ac.in>
2024-09-13 21:42:50 +0530

    made footer responsive and decreased width of scrollbar to 0

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,10 +12,10 @@ import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 
 const Footer = () => {
 	return (
-		<div className="w-full h-[500px] bg-[#19194D] flex flex-col">
-			<div className="flex p-2 w-full h-[90%] items-center">
-				<div className="w-[75%] h-4/5 border-r border-slate-700 pl-16 text-xl pr-8">
-					<div className="w-full h-[27px] mt-16 text-white flex">
+		<div className="w-full h-[500px] max-w-980:h-[750px] max-w-492:h-[1000px] bg-[#19194D] flex flex-col max-w-492:gap-8">
+			<div className="flex flex-row max-w-980:flex-col p-2 max-w-980:p-1 w-full h-[90%] items-center">
+				<div className="w-[75%] max-w-980:w-full h-4/5 border-r max-w-980:border-0 border-slate-700 pl-8 text-xl pr-8">
+					<div className="w-full h-[27px] mt-16 text-white flex max-w-980:flex-col max-w-980:gap-6">
 						<a
 							href="https://goo.gl/maps/KfBmHcSVKmzt3hvj7"
 							target="_blank"
@@ -23,16 +23,19 @@ const Footer = () => {
 						>
 							<LocationOnIcon /> IIIT Kota Permanent Campus, Kota, 325003
 						</a>
-						<a href="mailto:alumni@iiitkota.ac.in" className="ml-12">
+						<a
+							href="mailto:alumni@iiitkota.ac.in"
+							className="ml-8 max-w-980:ml-0"
+						>
 							<EmailIcon /> alumni@iiitkota.ac.in
 						</a>
 					</div>
-					<div className="w-full h-[27px] mt-6 text-white">
+					<div className="w-full h-[27px] mt-6 max-w-980:mt-20 text-white">
 						<a href="tel:0744-2667000">
 							<LocalPhoneIcon /> 0744-2667000, 0744-2667010
 						</a>
 					</div>
-					<div className="w-full h-[27px] mt-12 text-sm flex gap-16 border-b border-slate-700 pb-[3rem]">
+					<div className="w-full h-[27px] max-w-492:h-[150px] mt-12 text-sm flex max-w-492:flex-col gap-16 max-w-492:gap-2 border-b border-slate-700 pb-[3rem]">
 						<a href="" className="text-[#38B6FF]">
 							ABOUT <ArrowRightAltIcon />
 						</a>
@@ -46,7 +49,7 @@ const Footer = () => {
 							EVENTS <ArrowRightAltIcon />
 						</a>
 					</div>
-					<div className="w-full h-[27px] mt-8 text-sm flex gap-16 border-b border-slate-700 pb-[3rem]">
+					<div className="w-full h-[27px] max-w-492:h-[150px] mt-8 max-w-980:mb-8 text-sm flex max-w-492:flex-col gap-16 max-w-492:gap-2 border-b border-slate-700 pb-[3rem]">
 						<a href="" className="text-[#38B6FF]">
 							ABOUT <ArrowRightAltIcon />
 						</a>
@@ -61,9 +64,13 @@ const Footer = () => {
 						</a>
 					</div>
 				</div>
-				<div className="w-[25%] h-4/5 pl-8">
-					<img src={Logo} alt="" className="w-28 h-28" />
-					<p className="mt-4 mb-6 text-[1.65rem] leading-10 text-[#CED4DA]">
+				<div className="w-[25%] max-w-980:w-full h-4/5 pl-8 max-w-492:mb-16">
+					<img
+						src={Logo}
+						alt=""
+						className="w-28 h-28 max-w-980:w-24 max-w-980:h-24"
+					/>
+					<p className="mt-4 mb-6 text-[1.25rem] leading-10 text-[#CED4DA]">
 						ALUMNI CELL, <br /> INDIAN INSTITUTE OF INFORMATION TECHNOLOGY KOTA
 					</p>
 					<a
@@ -77,17 +84,17 @@ const Footer = () => {
 				</div>
 			</div>
 			<div className="w-full h-[10%] bg-[#0E407C] flex justify-center items-center">
-				<div className="w-full h-1/2 flex">
-					<div className="w-1/2 h-full flex justify-start items-center pl-8 text-white">
+				<div className="w-full h-1/2 max-w-492:h-full flex max-w-492:flex-col max-w-492:py-1">
+					<div className="w-1/2 h-full max-w-492:w-full max-w-492:h-1/3 flex justify-start max-w-492:justify-center items-center pl-8 max-w-492:pl-0 text-white max-w-492:text-sm">
 						© 2024 Alumni Cell, IIIT Kota
 					</div>
-					<div className="w-1/2 h-full flex justify-end items-center pr-4 gap-4">
-            <XIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
-            <FacebookIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
-            <InstagramIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
-            <LinkedInIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
-            <YoutubeIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
-          </div>
+					<div className="w-1/2 h-full max-w-492:w-full max-w-492:h-2/3 flex justify-end max-w-492:justify-center items-center pr-4 max-w-492:pr-0 max-w-492:gap-8 gap-4">
+						<XIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
+						<FacebookIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
+						<InstagramIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
+						<LinkedInIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
+						<YoutubeIcon className="text-white hover:text-[#38B6FF] transition-colors duration-300 ease-in-out cursor-pointer" />
+					</div>
 				</div>
 			</div>
 		</div>
