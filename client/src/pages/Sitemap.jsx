0d1385547cf7c7cf9ea82fde894616384517db1f
Coder Kaushik <2022kucp1077@iiitkota.ac.in>
2024-12-26 20:15:48 +0530

    fix(Sitemap): update Footer import path to use consistent casing

--- client/src/pages/Sitemap.jsx
+++ client/src/pages/Sitemap.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import Navbar from '../components/navbar';
-import Footer from '../components/footer';
+import Footer from '../components/Footer.jsx';
 
 const Sitemap = () => {
   const pages = [
